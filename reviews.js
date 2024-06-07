ee9c24b9161efc9352be6207a0c339daa64f4f3d
“[B <[li.bre@northeastern.edu]>
2024-06-07 15:19:34 -0400

    Added scaling for mobile.

--- reviews.js
+++ reviews.js
@@ -28,7 +28,7 @@ function create_content(data, user) {
         let div_name = `${element.Title.S}`;
         if (!document.getElementById(div_name)) {
             createNewDiv(div_name);
-            document.getElementById(div_name).innerHTML = `<h3 id="text">${element.Title.S}</h3><i><b><p id="text">"${element.Review.S}"</i></b><br> <em>${user} (${element.Rating.N}/10)<e/m></p>`;
+            document.getElementById(div_name).innerHTML = `<h3 id="text">${element.Title.S} (${element.Year.N})</h3><i><b><p id="text">"${element.Review.S}"</i></b><br> <em>${user} (${element.Rating.N}/10)<e/m></p>`;
         }
         else {
             document.getElementById(div_name).innerHTML += `<i><b><p id="text">"${element.Review.S}"</i></b><br> <em>${user} (${element.Rating.N}/10)<e/m></p>`;
@@ -64,7 +64,7 @@ function searchFunction() {
       if ((txtValue.toUpperCase().indexOf(filter) < 0) && (cl != 'topnav') && (id != 'search'))  {
         list[i].style.display = "none";
       } else {
-        list[i].style.display = "";
+        list[i].style.display = "none";
       }
     }
   }
